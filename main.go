970dfaad452b9f3f3f951ccba49e46b26fcf4b11
igorrochap <igor.systemof@gmail.com>
2022-12-17 20:38:19 -0300

    pointers

--- main.go
+++ main.go
@@ -13,4 +13,11 @@ func main() {
 	// account := Account{holder: "Igor", agencyNumber: 123, accountNumber: 123456, balance: 123.45}
 	account := Account{"Igor", 123, 123456, 123.45}
 	fmt.Println(account)
+
+	var otherAccount *Account
+	otherAccount = new(Account)
+	otherAccount.holder = "John"
+	otherAccount.balance = 500
+
+	fmt.Println(*otherAccount)
 }
